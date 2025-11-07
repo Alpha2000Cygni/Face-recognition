2c50bc2955bd00374a684eead7473f75a666c543
Alpha2000Cygni <alpha.cygni.2000@gmail.com>
2025-11-07 20:24:18 +0100

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ padding: '1rem', backgroundColor: '#1e293b', color: 'white' }}>
+      <h1>Gesichtserkennung</h1>
+    </header>
+  );
+};
+
+export default Header;
