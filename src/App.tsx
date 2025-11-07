b3232b49c53ad53b7fa78287a3e108d590521deb
Alpha2000Cygni <alpha.cygni.2000@gmail.com>
2025-11-07 20:23:39 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Willkommen zur Gesichtserkennungs-App</h2>
+        {/* Weitere Komponenten hier einfügen */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
